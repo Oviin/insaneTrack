68b17c430ba8cb85ea44901c0dfd8992af1c8429
Golang lead <you@example.com>
2017-02-05 20:49:44 +0300

    add forgotten files

---
+++ web/handlers/users.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "net/http"
+
+func AddUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+}
