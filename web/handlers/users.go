5f2dd19edd3bfecf10260ce5cab242eafc89fe84
Golang lead <you@example.com>
2017-02-05 21:19:04 +0300

    add handler for get all users

--- web/handlers/users.go
+++ web/handlers/users.go
@@ -13,3 +13,11 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func GetUsersList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+}
